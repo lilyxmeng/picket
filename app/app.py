8ce3f32bef68cc3fe755807ed92c911426f01cf8
Lily Meng <lilyxmeng13@gmail.com>
2021-08-21 23:51:29 -0400

    commit

--- app/app.py
+++ app/app.py
@@ -1,31 +1,24 @@
-from flask import Flask, render_template, Response, request, flash, redirect, url_for
-import cv2 
+from flask import Flask, render_template, Response, request, flash, redirect, url_for, jsonify
 import requests
 from PIL import Image
-import torch
-import torchvision
-from torch import nn
-import torch.nn.functional as F
-from torchvision.datasets import ImageFolder
-from torchvision.transforms import ToTensor
-import torchvision.transforms as transforms
 import numpy as np
 import io
 import os
 from werkzeug.utils import secure_filename
 
-
-app = Flask(__name__, static_folder="static")
-
+app = Flask(__name__)
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 UPLOAD_FOLDER = './static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/scan')
+def scan():
+    return render_template("scan.html")
 
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
@@ -33,12 +26,12 @@ def upload():
 
     if request.method == 'POST':
         if "file" not in request.files:
-            return render_template("prediction.html", prediction=prediction)
+            return render_template("scan.html", prediction=prediction)
 
         file = request.files['file']
 
         if file.filename == "":
-            return render_template("prediction.html", prediction=prediction)
+            return render_template("scan.html", prediction=prediction)
 
         if file:
             # Imports the Google Cloud client library
@@ -63,6 +56,8 @@ def upload():
 
             image = vision.Image(content=content)
 
+            classes =  ["cardboard", "glass", "metal", "paper", "plastic", "trash"]
+
             # Performs label detection on the image file
             response = client.label_detection(image=image)
             labels = response.label_annotations
@@ -71,20 +66,20 @@ def upload():
             for label in labels:
                 labels_list.append(label.description)
 
-            classes =  ["cardboard", "glass", "metal", "paper", "plastic", "trash"]
-
-            if "Glass" in labels_list:
-                prediction = "We predict this item belongs in the \"Glass\" bin"
-            elif "Metal" in labels_list:
-                prediction = "We predict this item belongs in the \"Metal\" bin"
-            elif "Plastic" in labels_list:
-                prediction = "We predict this item belongs in the \"Plastic\" bin"
-            elif "E-waste" in labels_list:
-                prediction = "We predict this item belongs in the \"E-Waste\" bin"
-            elif "Paper" in labels_list:
-                prediction = "We predict this item belongs in the \"Paper\" bin"
-            elif "Organic" in labels_list:
-                prediction = "We predict this item belongs in the \"Organic\" bin"
+            
+            for label in labels_list:
+                if "Glass" in label:
+                    prediction = "We predict this item belongs in the \"Glass\" bin."
+                elif "Metal" in label:
+                    prediction = "We predict this item belongs in the \"Metal\" bin."
+                elif "Plastic" in label:
+                    prediction = "We predict this item belongs in the \"Plastic\" bin."
+                elif "E-waste" in label:
+                    prediction = "We predict this item belongs in the \"E-Waste\" bin."
+                elif "Paper" in label:
+                    prediction = "We predict this item belongs in the \"Paper\" bin."
+                elif "Organic" in label:
+                    prediction = "We predict this item belongs in the \"Organic\" bin."
 
             print('Labels:')
             for label in labels:
@@ -92,10 +87,7 @@ def upload():
 
             print(prediction)
     
-    return render_template("prediction.html", prediction=prediction)
-
-
-
+    return render_template("scan.html", prediction=prediction)
 
 if __name__ == "__main__":
     app.run(debug=True)
